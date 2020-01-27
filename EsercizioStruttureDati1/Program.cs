7e8ca6f686a3a7175c9d2c7ff4fe7fedcf115268
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:47:27 +0100

    Add project files.

---
+++ EsercizioStruttureDati1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioStruttureDati1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> dati = new List<double>();
+            using (StreamReader r = new StreamReader("decimali.txt"))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    double n = double.Parse(line);
+                    dati.Add(n);
+                }
+            }
+            using (StreamWriter w = new StreamWriter("file.txt", false, Encoding.UTF8))
+            {
+                for(int i = dati.Count-1 ; i >=0 ; i--)
+                {
+                    w.WriteLine(dati[i]);
+                }
+            }
+        }
+    }
+}
